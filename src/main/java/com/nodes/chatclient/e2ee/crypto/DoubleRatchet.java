55aba986bd8718728e81897731f74ad775f6a594
bigdubz <siencebro@gmail.com>
2026-06-11 20:03:04 +0300

    test commit 3: offline session handshake idk what's up wit dat shit lowk

--- src/main/java/com/nodes/chatclient/e2ee/crypto/DoubleRatchet.java
+++ src/main/java/com/nodes/chatclient/e2ee/crypto/DoubleRatchet.java
@@ -185,15 +185,15 @@ public final class DoubleRatchet {
             long messageNumber,
             byte[] publicKey
     ) {
-//        byte[] labelBytes = MAIN_AD_LABEL.getBytes(StandardCharsets.UTF_8);
+        byte[] labelBytes = MAIN_AD_LABEL.getBytes(StandardCharsets.UTF_8);
+        byte[] channelBytes = ProtoOuterPayload.OuterPayload.Channel.CHAT.name().getBytes(StandardCharsets.UTF_8);
         byte[] senderBytes = senderId.getBytes(StandardCharsets.UTF_8);
         byte[] senderDeviceBytes = senderDeviceId.getBytes(StandardCharsets.UTF_8);
         byte[] receiverBytes = receiverId.getBytes(StandardCharsets.UTF_8);
         byte[] receiverDeviceBytes = receiverDeviceId.getBytes(StandardCharsets.UTF_8);
-        byte[] channelBytes = ProtoOuterPayload.OuterPayload.Channel.CHAT.name().getBytes(StandardCharsets.UTF_8);
 
         ByteBuffer buffer = ByteBuffer.allocate(
-//                4 + labelBytes.length +
+                4 + labelBytes.length +
                 4 + channelBytes.length +
                 4 + senderBytes.length +
                 4 + senderDeviceBytes.length +
@@ -203,8 +203,8 @@ public final class DoubleRatchet {
         );
         buffer.order(ByteOrder.BIG_ENDIAN);
 
-//        buffer.putInt(labelBytes.length);
-//        buffer.put(labelBytes);
+        buffer.putInt(labelBytes.length);
+        buffer.put(labelBytes);
 
         buffer.putInt(channelBytes.length);
         buffer.put(channelBytes);
